df0a2a3ae42dba46ccba0a7daa55c4765319ff81
Johnny Paige <paigejo@uw.edu>
2019-10-01 19:02:55 -0700

    fixed minor bug in SPDE model

--- modSPDE.R
+++ modSPDE.R
@@ -40,7 +40,7 @@ getSPDEMesh = function(locs, n=3500, max.n=5000, doPlot=TRUE, max.edge=c(.01, .1
 # function for fitting the SPDE model to gaussian data
 fitSPDE = function(obsCoords, obsValues, xObs=matrix(rep(1, length(obsValues)), ncol=1), 
                    predCoords, xPred = matrix(rep(1, nrow(predCoords)), ncol=1), 
-                   mesh=getSPDEMesh(obsCoords), spdePrior=getSPDEPrior(mesh), 
+                   mesh=getSPDEMesh(obsCoords), prior=getSPDEPrior(mesh), 
                    significanceCI=.8, int.strategy="grid", strategy="laplace", 
                    nPostSamples=1000, verbose=TRUE, link=1, seed=NULL) {
   
