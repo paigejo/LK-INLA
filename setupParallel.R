6bd3b62f6ad41140603092d2b85aeac0911f6608
Johnny Paige <paigejo@uw.edu>
2019-10-05 00:26:02 -0700

    fixed parallel setup for Windows

--- setupParallel.R
+++ setupParallel.R
@@ -4,12 +4,17 @@ assign("doParallel", TRUE, envir=.GlobalEnv)
 library(parallel)
 library(doParallel)
 library(foreach)
-setwd("~/git/LK-INLA/")
-source("setup.R")
+inf = sessionInfo()
+if(inf$platform != "x86_64-w64-mingw32/x64 (64-bit)") {
+  setwd("~/git/LK-INLA/")
+  source("setup.R")
+} else {
+  setwd("U:/git/LK-INLA/")
+  source("setup.R")
+}
 
 # assign("cl", detectCores() - 1, envir=.GlobalEnv)
 assign("cl", makeCluster(7, outfile="log.txt"), envir=.GlobalEnv)
-inf = sessionInfo()
 if(inf$platform != "x86_64-w64-mingw32/x64 (64-bit)") {
   clusterEvalQ(cl, {setwd("~/git/LK-INLA/"); source("setup.R")})
 } else {
